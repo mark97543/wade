6b758e0ca3d53d685fc578adc8ffd14f641e2533
Mark Wade <wade.mark.a@gmail.com>
2025-06-03 14:40:58 -0600

    Imported 404 page and video from previous build.

--- 1_client/src/App.jsx
+++ 1_client/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '@wade-usa/components/header/header.jsx'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Page404 from './pages/404 page/404NotFound';
 
 function App() {
 
   return (
     <div className='wrapper'>
       <Header/>
+      <Router>
+        <Routes>
+          
+          <Route path='/*' element={<Page404/>}/> {/* 404 Page for all pages that do not exist*/}
+        </Routes>
+      </Router>
 
-      <h1>Need to Design Home Page</h1>
+      <h1>Not Logged In</h1>
 
 
     </div>
---
+++ 1_client/src/pages/404 page/404NotFound.jsx
@@ -0,0 +1,33 @@
+// /client/src/pages/404 page/404NotFound.jsx
+
+import React from "react";
+import './404NotFound.css'
+
+const Page404 = ()=>{
+
+    return(
+        <>
+            <div className="error-text-404">
+                <h1>Error: 404 Page not found</h1>
+            </div>
+            <div className="P404-video">
+                    <video
+                        width="100%" // Example: Make it responsive width
+                        preload="metadata" // Helps load dimensions/duration quickly
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                    >
+                        <source src='https://01-spaces.sfo3.cdn.digitaloceanspaces.com/5907755f-aee4-48cf-8571-8dfae92be9e6.mp4' type="video/mp4" />
+                        Your browser does not support the video tag. Please update your browser.
+                    </video>
+            </div>
+        
+        </>
+    )
+
+
+}
+
+export default Page404;
