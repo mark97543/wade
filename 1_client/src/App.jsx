424a160d86a908583d5d05c8b708c48cea871e36
Mark Wade <wade.mark.a@gmail.com>
2025-06-04 18:06:09 -0600

    Added Second Site to Try and See how to link and p

--- 1_client/src/App.jsx
+++ 1_client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className='wrapper'>
-      <Router basename='/home'>
+      <Router basename='/'>
         <AuthProvider>
           <Header/>
           <Routes>
