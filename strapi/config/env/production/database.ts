9b663ee2e9701b6604c2c1e24208b60a3610889b
Mark Wade <wade.mark.a@gmail.com>
2025-06-08 15:31:19 -0600

    Missed one

---
+++ strapi/config/env/production/database.ts
@@ -0,0 +1,15 @@
+// path: strapi/app/config/env/production/database.ts
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false), // Set to true if you are using a managed database with SSL
+    },
+    debug: false,
+  },
+});
